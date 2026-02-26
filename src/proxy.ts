d10f06c70d91fec07515a0dc139d84468388a3a3
phuong130199 <ngocphuong0342591999@gmail.com>
2026-02-26 22:43:55 +0700

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -86,12 +86,16 @@ export const proxy = async (req: NextRequest) => {
     const ip = req.headers.get('x-nf-client-connection-ip') || req.headers.get('x-forwarded-for')?.split(',')[0].trim() || 'unknown';
 
     if (!ua || BLOCKED_UA_REGEX.test(ua)) {
+        console.log(ua)
+        console.log(ip)
         return new NextResponse(null, { status: 404 });
     }
 
     if (ip !== 'unknown') {
         const geoInfo = await getGeoInfo(ip);
         if (geoInfo) {
+            console.log(ua)
+            console.log(ip)
             if (geoInfo.asn && BLOCKED_ASN.has(geoInfo.asn)) {
                 return new NextResponse(null, { status: 404 });
             }
